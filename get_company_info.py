0edd11b266ade4feae9671dc3ff77a04c15ad983
NikitaMulyar <nickm.school@mail.ru>
2024-01-27 14:52:52 +0300

    5+2 Russia/USA companies with new format

--- get_company_info.py
+++ get_company_info.py
@@ -21,7 +21,7 @@ def get_info(company, period, total):
         minutes = total * 60 * 24 * 92
     tick = Ticker(company)
     today = date.today()
-    fromday = today - timedelta(minutes=minutes) - timedelta(minutes=60 * 24 * 95)  # Вычитаем квартал
+    fromday = today - timedelta(minutes=minutes) - timedelta(minutes=60 * 24 * 95 * 10)  # Вычитаем квартал
     df = pd.DataFrame(tick.candles(date=fromday, till_date=today, period=moex_periods[period]))
     df = df[:total]
     df = df[['begin', 'open', 'close', 'high', 'low']]
@@ -32,7 +32,7 @@ def get_info(company, period, total):
 def get_usa_company_info(company, period, total):
     tick = yf.Ticker(company)
     df = tick.history(interval=yfinances_periods[period], period="max")
-    df = df[:total]
+    df = df.tail(total)
     df.columns = [x.lower() for x in df.columns]
     df = df[['open', 'close', 'high', 'low']]
     df[['open', 'close', 'high', 'low']] = df[['open', 'close', 'high', 'low']].apply(lambda x: round(x, 2))
@@ -43,11 +43,11 @@ def get_usa_company_info(company, period, total):
 
 
 if __name__ == '__main__':
-    """get_info('SBER', 'D', 500)
+    get_info('SBER', 'D', 500)
     get_info('MOEX', 'D', 500)
     get_info('YNDX', 'D', 500)
     get_info('DSKY', 'D', 500)
-    get_info('LKOH', 'D', 500)"""
+    get_info('LKOH', 'D', 500)
     get_usa_company_info("META", 'D', 500)
     get_usa_company_info("AAPL", 'D', 500)
 
