941c8d90f36a3e58599ea716d1606d798b8c18c0
progetto65 <tmandzhiev2@gmail.com>
2024-01-27 12:07:19 +0300

    фикс багов

--- strategy/compression.py
+++ strategy/compression.py
@@ -1,11 +1,18 @@
 import csv
 import os
 
-
 file = open("../csv_files/YNDX.csv", encoding="utf8")
-reader = csv.reader(file, delimiter=";", quotechar='"')
-for line in reader:
-    #print(str(*line))
-    number, time_begin, open, close, high, low, value, volume, time_end = str(*line).split(',')
-    print(number, time_begin, open, close, high, low, value, volume, time_end, sep='\n')
-file.close()
+
+
+def compress_csv():
+    reader = csv.reader(file, delimiter=";", quotechar='"')
+
+    for line in reader:
+        # print(str(*line))
+        number, time_begin, open, close, high, low, value, volume, time_end = str(*line).split(',')
+        if number:
+            print(number, time_begin, open, close, high, low, value, volume, time_end, sep='\n')
+    file.close()
+
+
+compress_csv()
