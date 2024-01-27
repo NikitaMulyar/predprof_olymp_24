a523ba87311c9787c0c2d5b5ab1a7327b6687a1d
arthurmardanshin <mardartu@mail.ru>
2024-01-27 14:19:56 +0300

    относительно не тупая стратегия

--- strategy/delete_absorption.py
+++ strategy/delete_absorption.py
@@ -3,7 +3,7 @@ import os
 import random
 
 def absorption(size):
-    file = open("../csv_files/MOEX.csv", encoding="utf8")
+    file = open("../csv_files/SBER.csv", encoding="utf8")
     reader = csv.reader(file, delimiter=";", quotechar='"')
     A = [[] for _ in range(size)]
     i = 0
@@ -17,9 +17,9 @@ def absorption(size):
         i += 1
         if (i > size):
             break
-    BUY = []  # все цены, которые попадают в этот массив, отмечаются зеленой галочкой на графике
-    BUY_FOR_PROJECT = [] #
-    SELL = []  # все цены, которые попадают в этот массив, отмечаются красным ротиком на графике
+    BUY = []
+    BUY_FOR_PROJECT = [] # итоговые покупки в формате: цена покупки, дата покупки, объем, процент убыли/прибыли
+    SELL = []  # тоже, что BUY_FOR_PROJECT только с продажами в формате: цена, дата, объем
     up = 0
     budget = 0
     for i in range(2, len(A)):
@@ -38,7 +38,7 @@ def absorption(size):
         if absort:
             if (A[i - 2][2] < A[i - 2][1]) and (A[i - 1][2] > A[i - 1][1]):  # черная свеча поглощена белой. Следовательно, сегодня покупаем
                 d = int(1 + (max_b - min_b) / (max_a - min_a))
-                BUY_FOR_PROJECT.append([A[i][1], A[i][0], 1, 0])
+                BUY_FOR_PROJECT.append([A[i][1], A[i][0], d, 0])
                 BUY.append([A[i][1], A[i][0], d, 0])  # цена покупки, дата покупки, объем, процент прибыли/убыли
                 up -= A[i][1] * d
                 budget += A[i][1] * d
@@ -52,14 +52,17 @@ def absorption(size):
                 if value_to_sell != 0:
                     SELL.append([A[i][1], A[i][0], value_to_sell])  # цена продажи, дата продажи, объем
                 if (i > 5) and (A[i - 5][2] > (A[i - 1][1] + A[i - 1][2]) / 2) and (abs(A[i - 1][1] - A[i - 1][2]) * 4 < A[i - 1][4] - A[i - 1][3]):
-                    BUY.append(A[i][1], A[i][0], 1, 0) # молот
-                    up -= A[i][1]
-                    budget += A[i][1]
+                    d = (A[i - 1][4] - A[i - 1][3]) / (abs(A[i - 1][1] - A[i - 1][2]))
+                    BUY.append(A[i][1], A[i][0], d, 0) # молот
+                    BUY_FOR_PROJECT.append([A[i][1], A[i][0], d, 0])
+                    up -= A[i][1] * d
+                    budget += A[i][1] * d
         print(A[i])
         print(A[i][3])
-        print("BUY")
-        print(*BUY, sep='\n')
         print("SELL")
+        print(*SELL, sep='\n')
+        print("BUY_FOR_PROJECT")
+        print(*BUY_FOR_PROJECT, sep='\n')
         print(*SELL, sep='\n')
         print()
     file.close()
